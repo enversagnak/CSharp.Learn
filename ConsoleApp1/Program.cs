c1edd51a94035172e67eac6f4749fd51deb8509e
Enver Sağnak <enver_543@hotmail.com>
2017-11-02 13:32:53 +0300

    adsad

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HesapMakinesi
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            {
+
+
+
+                  int getKelimeSayisi()
+                    {
+                    return cumle.Split(' ').Length; }
+
+
+
+                    Console.WriteLine("Bir cümle giriniz.");
+
+                    string cumle = Console.ReadLine();
+
+                    for (int i = 0; i < getKelimeSayisi.ToString; i++)
+                    {
+                      
+
+                    }
+                    
+
+                    
+
+                    Console.ReadLine();
+
+
+                
+
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+
+}
+            
